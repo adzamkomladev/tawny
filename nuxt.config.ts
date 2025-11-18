bd1d281aa4ba2081258d429e2a818dea69aba260
Komla Adzam <adzamkomla.dev@gmail.com>
2025-11-18 13:07:48 +0000

    feat: add shadcn components integration and configure Tailwind CSS
    - Created components.json for shadcn configuration with Tailwind CSS settings.
    - Updated nuxt.config.ts to include shadcn-nuxt and Tailwind CSS plugin.
    - Added necessary dependencies in package.json for shadcn, Tailwind CSS, and related libraries.
    - Modified tsconfig.json to set up path aliases for TypeScript.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import tailwindcss from '@tailwindcss/vite';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -9,8 +11,31 @@ export default defineNuxtConfig({
     '@nuxt/icon',
     '@nuxt/image',
     '@nuxt/scripts',
-    '@nuxthub/core'
+    '@nuxthub/core',
+    'shadcn-nuxt',
   ],
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component.
+     * @default "Ui"
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: '@/components/ui'
+  },
+
   nitro: {
     experimental: {
       websocket: true
